4b863c18ecbc18399f97493cd45d034c8826c20c
DevJag <devjag.linux@gmail.com>
2022-05-08 20:41:53 -0300

    Months of the year algorithm

---
+++ monthsYear.py
@@ -0,0 +1,9 @@
+months = ["This month does not exist.", "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+month = int(input("Enter an month: "))
+if True:
+    print("Month: %s" % months[month])
+elif month == 0:
+    print(months[0])
+else:
+    print("Enter an value integer")
+   
